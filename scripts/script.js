ffff2317b65d81cf5c78b907afe5c2891299b659
jessica <jessicagamper@gmail.com>
2022-12-13 15:04:47 +0100

    created a Navbar wtih subpages and finished design for banner

--- scripts/script.js
+++ scripts/script.js
@@ -3,38 +3,37 @@ let button = document.querySelector("button");
 function TimerFunction() {
     console.log("Timer has Began!");
 
-    let endOfTimer = Date.parse("Dec 7, 2022 15:30:00")//Hard coded countdown end point
+    let endOfTimer = Date.parse("Dec 7, 2022 15:30:00"); //Hard coded countdown end point
+
     let nowTime = new Date(); //Getting a copy of the entire date and time from user
+
     let timeDifference = endOfTimer - nowTime; //Future End time minus Now Time
 
-    console.log(timeDifference) //test
+    console.log(timeDifference); //Test
 
-    let days = Math.floor(timeDifference /(1000 * 60 * 60 * 24));
-    let hours = Math.floor(timeDifference /(1000 * 60 * 60));
-    let minutes = Math.floor(timeDifference / (1000 * 60))
-    let seconds = Math.floor(timeDifference / 1000)
-    console.log(seconds + "\n" + minutes + "\n" + hours + "\n" + days)
+    let days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    let hours = Math.floor(timeDifference / (1000 * 60 * 60));
+    let minutes = Math.floor(timeDifference / (1000 * 60));
+    let seconds = Math.floor(timeDifference / 1000);
+    console.log(seconds + "\n" + minutes + "\n" + hours + "\n" + days);
+    console.log(days + 'd', hours + 'h', minutes + 'min', seconds + 'sec'); //From Ivan
+
     let dayTime = days;
     let hourTime = hours - days * 24;
     let minuteTime = minutes - hours * 60;
-    let secondTime = seconds - minutes * 60;
-    
-    console.log (dayTime, hourTime, minuteTime, secondTime)
-    document.querySelector(".timer").innerHTML = '<div>' + dayTime + '</div>' +
-        '<div>' + hourTime + '</div>' +
-        '<div>' + minuteTime + '</div>' +
-        '<div>' + secondTime + '</div>';
-    
+    let secondsTime = seconds - minutes * 60;
 
+    console.log(dayTime, hourTime, minuteTime, secondsTime);
+
+    document.querySelector(".timer").innerHTML = //Injecting the Code
+      '<div>' + dayTime + '</div>' +
+      '<div>' + hourTime + '</div>' +
+      '<div>' + minuteTime + '</div>' +
+      '<div>' + secondsTime + '</div>';
 }
-
-setInterval(
-    'TimerFunction()', //code to be triggered after delay time is up
-    1000); //delay
-
-TimerFunction();
 
 
 button.addEventListener("click", () => {
-    TimerFunction();
+    // TimerFunction();
+    setInterval('TimerFunction()', 1000);//code to be triggerd after delay time is up with delay
 })
